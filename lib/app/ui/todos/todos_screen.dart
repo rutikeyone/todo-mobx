648e5a6b3dfb4d9ac71480719f3c654287e13657
rutikeyone <rutikeyone@gmail.com>
2022-08-16 21:17:57 +0700

    Init project

---
+++ lib/app/ui/todos/todos_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:to_do/app/ui/todos/todos_page.dart';
+
+class TodosScreen extends StatelessWidget {
+  const TodosScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const TodosPage();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:to_do/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
